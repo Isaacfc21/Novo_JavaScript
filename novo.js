057c8eb67ee7dcd2a2f8f0dd2daa96fc80ee9f44
Isaac <isaaccaracayahoo@gmail.com>
2025-02-22 16:53:55 -0300

    Java Script

--- novo.js
+++ novo.js
@@ -1,93 +1,125 @@
-const btn_cancelar = document.querySelector("#btn_cancelar")
-const elementosForm = document.querySelector("#elementosForm")
-const adicionar_c = document.querySelector(".adicionar_c")
-const btn_gravar = document.querySelector("#btn_gravar")
-const add_circle = document.querySelector("#add_circle")
+const btn_cancelar = document.querySelector("#btn_cancelar");
+const elementosForm = document.querySelector("#elementosForm");
+const adicionar_c = document.querySelector(".adicionar_c");
+const btn_gravar = document.querySelector("#btn_gravar");
+const add_circle = document.querySelector("#add_circle");
 
 const configdgv = {
     endpoint: "http://127.0.0.1:1880/dados_usuario",
 }
 
 const dgv_usuario = (configdgv) => {
-    const dgvUsurio = document.getElementById("dgvUsuario")
-    const dgvTitulos = document.createElement("div")
-    dgvTitulos.setAttribute("id", "dgvTitulos")
-    dgvTitulos.setAttribute("class", "dgvTitulos")
+    const dgvUsuario = document.getElementById("dgvUsuario");
+    let dgvDados = document.getElementById("dgvDados");
 
-    const id = document.createElement("div")
-    id.setAttribute("class", "c1")
-    id.innerHTML="ID"
-    dgvTitulos.appendChild(id)
+        if (!dgvDados) {
+            dgvDados = document.createElement("div");
+            dgvDados.setAttribute("id", "dgvDados");
+            dgvUsuario.appendChild(dgvDados);
+        } else {
+            dgvDados.innerHTML = ""; // Limpa os dados antigos
+        }
+    
+        dgvUsuario.innerHTML="";
 
-    const nome = document.createElement("div")
-    nome.setAttribute("class", "c2")
-    nome.innerHTML="Nome"
-    dgvTitulos.appendChild(nome)
+        const dgvTitulos = document.createElement("div");
+        dgvTitulos.setAttribute("id", "dgvTitulos");
+        dgvTitulos.setAttribute("class", "dgvTitulos");
 
-    const celular = document.createElement("div")
-    celular.setAttribute("class", "c3")
-    celular.innerHTML="Celular"
-    dgvTitulos.appendChild(celular)
+        const id_usuario = document.createElement("div");
+        id_usuario.setAttribute("class", "c1");
+        id_usuario.innerHTML = "ID";
+        dgvTitulos.appendChild(id_usuario);
 
-    const email = document.createElement("div")
-    email.setAttribute("class", "c4")
-    email.innerHTML="Email"
-    dgvTitulos.appendChild(email)
+        const nome_usuario = document.createElement("div");
+        nome_usuario.setAttribute("class", "c2");
+        nome_usuario.innerHTML = "Nome";
+        dgvTitulos.appendChild(nome_usuario);
 
-    const data_n = document.createElement("div")
-    data_n.setAttribute("class", "c5")
-    data_n.innerHTML="Data de Nascimento"
-    dgvTitulos.appendChild(data_n)
+        const celular_usuario = document.createElement("div");
+        celular_usuario.setAttribute("class", "c3");
+        celular_usuario.innerHTML = "Celular";
+        dgvTitulos.appendChild(celular_usuario);
 
-    dgvUsurio.prepend(dgvTitulos)
+        const email_usuario = document.createElement("div");
+        email_usuario.setAttribute("class", "c4");
+        email_usuario.innerHTML = "Email";
+        dgvTitulos.appendChild(email_usuario);
 
-    dgvDados.innerHTML="";
+        const data_n_usuario = document.createElement("div");
+        data_n_usuario.setAttribute("class", "c5");
+        data_n_usuario.innerHTML = "Data de Nascimento";
+        dgvTitulos.appendChild(data_n_usuario);
+
+        dgvUsuario.appendChild(dgvTitulos);
+
+
     fetch(configdgv.endpoint)
     .then(res=>res.json())
     .then(res =>{
         console.log(res)
+        res.reverse();
         res.forEach(el=>{
            const dgvLinha = document.createElement("div");
            dgvLinha.setAttribute("class", "dgvLinha");
            dgvDados.appendChild(dgvLinha);
+           dgvUsuario.appendChild(dgvDados);
 
            const c1 = document.createElement("div");
            c1.setAttribute("class", " coluna c1");
-           c1.innerHTML=el.n_id_produto;
+           c1.innerHTML=el.n_id_usuario;
            dgvLinha.appendChild(c1);
 
            const c2 = document.createElement("div");
            c2.setAttribute("class", " coluna c2");
-           c2.innerHTML=el.s_nome_produto;
+           c2.innerHTML=el.s_nome_usuario;
            dgvLinha.appendChild(c2);
 
            const c3 = document.createElement("div");
            c3.setAttribute("class", "coluna c3");
-           c3.innerHTML=el.s_marca_produto;
+           c3.innerHTML=el.s_telefone_usuario;
            dgvLinha.appendChild(c3);
 
            const c4 = document.createElement("div");
            c4.setAttribute("class", "coluna c4");
-           c4.innerHTML=el.s_modelo_produto;
+           c4.innerHTML=el.s_email_usuario;
            dgvLinha.appendChild(c4);
 
            const c5 = document.createElement("div");
            c5.setAttribute("class", "coluna c5");
+           c5.innerHTML=el.s_data_usuario;
            dgvLinha.appendChild(c5);
         });
     });
 };
 
-btn_cancelar.addEventListener("click", (evt)=> {
-    elementosForm.setAttribute("class", "ocultar")
-})
+btn_gravar.addEventListener("click", (evt) => {
+    elementosForm.setAttribute("class", "ocultar");
+    // adicionar_c.remove();
 
-btn_gravar.addEventListener("click", (evt)=> {
-    elementosForm.setAttribute("class", "ocultar")
-    adicionar_c.remove()
-    dgv_usuario(configdgv)
-})
+    const id = document.querySelector("#f_id").value;
+    const nome = encodeURIComponent(document.querySelector("#f_nome").value);
+    const telefone = encodeURIComponent(document.querySelector("#f_celular").value);
+    const email = encodeURIComponent(document.querySelector("#f_email").value);
+    const data = encodeURIComponent(document.querySelector("#f_dtnasc").value);
+
+    const endpoint = `http://127.0.0.1:1880/addusuario/${id}/${nome}/${telefone}/${email}/${data}`;
+    
+    fetch(endpoint)
+        .then(res => {
+            if (res.status == 200) {
+                dgv_usuario(configdgv);
+            } else {
+                alert("Erro ao Calcular");
+            }
+        });
+});
+
+btn_cancelar.addEventListener("click", (evt)=> {
+    elementosForm.setAttribute("class", "ocultar");
+});
 
 add_circle.addEventListener("click", (evt)=> {
-    elementosForm.setAttribute("class", "elementosForm")
-})
+    elementosForm.setAttribute("class", "elementosForm");
+});
+dgv_usuario(configdgv)
